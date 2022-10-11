ea60b90f77bb49145b91708716c5cb3d19908b27
shifas <shifas@webcastle.in>
2022-10-11 15:08:51 +0530

    Custom pagination with next and previous button

--- src/app/pages/catalog/brand/brand-card/brand-card.component.ts
+++ src/app/pages/catalog/brand/brand-card/brand-card.component.ts
@@ -15,23 +15,32 @@ export class BrandCardComponent implements OnInit {
   appRoute = appRoutes;
   brands: any;
   base: any
+  data: any
 
   //Page and limit for query
   page: any = 1;
+  pages: any = []
+  nextpages: any = []
   currpage: any = 1;
   limit: any = 8;
+  selectedpage: any = 1
+  max: any = 3
 
   //Total no. of data from backend
   totalcount: any;
+  totaldata: any;
   count: any = 0
 
   //Conditions
   isData: boolean = true;
   showBtn: boolean = true;
   showLessBtn: boolean = false;
+  isNext: boolean = true
 
   //Filters array
   filters: any = [];
+  show: any;
+  shifted: any
 
   constructor(
     private brandService: BrandService,
@@ -42,6 +51,9 @@ export class BrandCardComponent implements OnInit {
   ngOnInit(): void {
     this.initForm()
     this.base = environment.base
+    setTimeout(() => {
+      this.setPages()
+    })
 
     this.brandService.getBrands(this.page, this.limit).subscribe((res: any) => {
       this.brands = res?.result
@@ -51,7 +63,9 @@ export class BrandCardComponent implements OnInit {
 
     this.brandService.getBrandCount().subscribe((res: any) => {
       this.totalcount = res?.result
+      this.totaldata = Math.ceil(this.totalcount / this.limit)
       this.cdr.markForCheck();
+      this.setPages()
     })
   }
 
@@ -67,55 +81,88 @@ export class BrandCardComponent implements OnInit {
     window.location.reload()
   }
 
-  searchBrand(key: any, e: any) {
+  searchBrand() {
     this.currpage = 1
     this.brandService.searchBrand(this.brandform.value, this.page, this.limit).subscribe((res: any) => {
       if (res?.errorCode == 0) {
         this.brands = res?.result?.data
         this.count = this.brands.length
+        this.totalcount = res?.result?.total
+        this.totaldata = Math.ceil(this.totalcount / this.limit)
+        this.setPages()
         this.cdr.markForCheck();
         this.isData = true
-        this.totalcount = res?.result?.total
-        this.setBoolValues(this.totalcount, this.brands.length)
       }
     })
   }
 
-  fetchMore() {
+  fetchBrand(page: any, limit: any) {
+    this.selectedpage = page
+    this.currpage = page
+    this.getData(this.brandform.value, page, limit)
+  }
+
+  loadNext() {
     this.currpage += 1
-    this.brandService.searchBrand(this.brandform.value, this.currpage, this.limit).subscribe((res: any) => {
-      this.brands = [...this.brands, ...res?.result.data]
-      this.count = this.brands.length
-      this.setBoolValues(this.totalcount, this.brands.length)
-      this.cdr.markForCheck();
-    })
-    if (this.currpage > 1) {
-      this.showLessBtn = true
+    this.selectedpage += 1
+    if (this.currpage <= 3) {
+      if (this.currpage <= this.totaldata) {
+        this.getData(this.brandform.value, this.currpage, this.limit)
+      } else {
+        this.isNext = false
+      }
+    } else {
+      this.shifted = this.pages.shift() //Captures the shifted number from pagination array
+      this.pages.push(this.currpage)
+      if (this.currpage <= this.totaldata) {
+        this.getData(this.brandform.value, this.currpage, this.limit)
+      } else {
+        this.isNext = false
+      }
     }
   }
 
-  fetchLess() {
+  loadPrevious() {
+    this.currpage -= 1
+    this.selectedpage -= 1
+    if (this.currpage > 3 && this.currpage <= this.totaldata && this.currpage > 0) {
+      this.getData(this.brandform.value, this.currpage, this.limit)
+    }
+    else {
+      if (this.pages[0] != 1) {
+        this.pages.pop()
+        this.pages.unshift(this.shifted)
+        this.shifted -= 1
+        this.getData(this.brandform.value, this.currpage, this.limit)
+      } else {
+        this.getData(this.brandform.value, this.currpage, this.limit)
+      }
+    }
+  }
+
+  setPages() {
     this.currpage = 1
-    this.brandService.searchBrand(this.brandform.value, this.currpage, this.limit).subscribe((res: any) => {
-      this.brands = res?.result?.data
-      this.count = this.brands.length
-      this.setBoolValues(this.totalcount, this.brands.length)
-      this.cdr.markForCheck();
-    })
+    this.selectedpage = 1
+    this.pages.length = 0
+    if (this.totaldata > 3) {
+      for (let i = 1; i <= this.max; i++) {
+        this.pages.push(i)
+      }
+    } else {
+      for (let i = 1; i <= this.totaldata; i++) {
+        this.pages.push(i)
+      }
+    }
   }
 
-  setBoolValues(datalen: any, brandlen: any) {
-    if (datalen == brandlen) {
-      this.showBtn = false
-      this.showLessBtn = true
-    } else {
-      this.showBtn = true
-      this.showLessBtn = false
-    }
-    if (brandlen == 0) {
-      this.isData = false
-    } else {
-      this.isData = true
-    }
+  getData(data: any, page: any, limit: any) {
+    this.brandService.searchBrand(data, page, limit).subscribe((res: any) => {
+      if (res?.errorCode == 0) {
+        this.brands = res?.result?.data
+        this.count = this.brands.length
+        this.cdr.markForCheck();
+      }
+    })
+    this.isNext = true
   }
 }
