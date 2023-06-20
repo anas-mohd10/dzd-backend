4d03ae91b0827ba33d94702fca4eb8e491c778ff
Dheemanth Shenoy <dheemanth@webcastle.in>
2023-06-20 13:23:30 +0530

    Cart page optimized

--- src/app/pages/sales/cart/cart-list/cart-list.component.ts
+++ src/app/pages/sales/cart/cart-list/cart-list.component.ts
@@ -1,11 +1,7 @@
-import { Component, OnInit, OnDestroy, ViewChild, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
 import { appRoutes } from 'src/app/config/routes/app.routes';
-import { DataTableDirective } from 'angular-datatables'
-import { Subject } from 'rxjs';
 import { CartService } from 'src/app/includes/services/cart.service';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
-import { CustomersService } from 'src/app/includes/services/customers.service';
-import { ProductService } from 'src/app/includes/services/product.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { ToastrService } from 'ngx-toastr';
 
 @Component({
@@ -14,122 +10,110 @@ import { ToastrService } from 'ngx-toastr';
   styleUrls: ['./cart-list.component.scss']
 })
 
-export class CartListComponent implements OnInit, OnDestroy {
-  @ViewChild(DataTableDirective, { static: false })
-  public dtElement: DataTableDirective;
-  public dtOptions: DataTables.Settings = {};
-  public dtTrigger: Subject<any> = new Subject();
-
+export class CartListComponent implements OnInit {
   appRoute = appRoutes
-  cartsData: any
-  cartForm: FormGroup;
-
-  customers: any = [];
-  customer: any
-
-  products: any = [];
-  product: any
-
-  status = new FormControl('')
-  isErrors: any = false
-  carts: any = []
-  isTable: Boolean = false
-  user: any = ''
-  form: any
-  message: any = new FormControl('', Validators.required)
-  couponCode: any = new FormControl('')
-  mobile: any
+  form: FormGroup
+  keyword: FormControl = new FormControl('')
+  isActive: FormControl = new FormControl('')
+  type: FormControl = new FormControl('')
+  fromDate: FormControl = new FormControl('')
+  toDate: FormControl = new FormControl('')
+  page: number = 1
+  limit: FormControl = new FormControl(20)
+  lastPage: Boolean = false
+  carts: Array<any> = []
+  isSubmitted: Boolean = true
+  cart: any = {}
 
   constructor(
     private cartService: CartService,
-    private formBuilder: FormBuilder,
-    private productService: ProductService,
-    private customerService: CustomersService,
     private cdr: ChangeDetectorRef,
     private ToastrService: ToastrService
   ) { }
 
+  get fc() {
+    return this.form.controls
+  }
+
   ngOnInit(): void {
-    this.dtOptions = {
-      pagingType: 'simple_numbers',
-      lengthMenu: [5, 10, 15],
-      pageLength: 10,
-      processing: true,
-    };
-
-    this.customerService.getActiveCustomers().subscribe((res: any) => {
-      this.customers = res?.result
-      this.cdr.markForCheck()
-    })
-
-    this.cartService.getCarts({}).subscribe((res: any) => {
-      this.carts = res?.result
-      for (let cart of this.carts) {
-        cart.date.added = new Date(cart?.date?.added).toDateString()
-      }
-      this.cdr.markForCheck()
-      this.dtTrigger.next();
-    })
-
-    this.form = new FormGroup({
-      message: new FormControl('', Validators.required),
-      title: new FormControl('', Validators.required),
-      couponCode: new FormControl('', Validators.required)
-    })
+    this.seachCart()
+    this.initForm()
   }
 
   clearFilter() {
-    this.customer = null
-    this.customerService.getActiveCustomers().subscribe((res: any) => {
-      if (res?.errorCode == 0) { }
-      this.customers = res?.result
-      this.cdr.markForCheck()
-    })
+    this.isActive.setValue('')
+    this.keyword.setValue('')
+    this.type.setValue('')
+    this.fromDate.setValue('')
+    this.toDate.setValue('')
+    this.page = 1
+    this.limit.setValue(20)
+    this.seachCart()
+  }
 
-    this.cartService.getCarts({}).subscribe((res: any) => {
+  getPreviousPage() {
+    this.page -= 1
+    this.seachCart()
+  }
+
+  getNextPage() {
+    this.page += 1
+    this.seachCart()
+  }
+
+  seachCart() {
+    let payload = {
+      page: this.page,
+      limit: this.limit.value,
+      keyword: this.keyword.value,
+      type: this.type.value,
+      fromDate: this.fromDate.value,
+      toDate: this.toDate.value,
+      isActive: this.isActive.value
+    }
+
+    this.cartService.getCarts(payload).subscribe((res: any) => {
       if (res?.errorCode == 0) {
-        this.carts = res?.result
-        for (let cart of this.carts) {
-          cart.date.added = new Date(cart?.date?.added).toDateString()
-        }
+        this.carts = res?.result?.data
+        this.lastPage = res?.result?.lastPage
+        this.page = res?.result?.page
         this.cdr.markForCheck()
       }
     })
   }
 
-  seachCart() {
-    let body = { customer: this.customer }
-    this.cartService.getCarts(body).subscribe((res: any) => {
-      this.carts = res?.result
-      this.cdr.markForCheck()
+  initForm() {
+    this.form = new FormGroup({
+      title: new FormControl('', Validators.required),
+      message: new FormControl('', Validators.required),
+      couponCode: new FormControl('')
     })
-  }
-
-  ngOnDestroy(): void {
-    this.dtTrigger.unsubscribe();
-  }
-
-  getUser(user: any, mobile: any) {
-    this.user = user
-    this.mobile = mobile
   }
 
   closeModal() {
     this.form.reset()
+    this.cart = {}
+    this.isSubmitted = true
+  }
+
+  openModal(cart: any) {
+    this.cart = cart
   }
 
   sendPush() {
     if (!this.form.valid) {
-      this.isErrors = true
+      this.isSubmitted = false
       return
     }
 
-    let payload = { title: this.form.get('title')?.value, message: this.form.get('message')?.value, couponCode: this.form.get('couponCode')?.value, mobile: this.mobile, countryCode: "+91" }
+    let payload = {
+      ...this.form.value,
+      refid: this.cart?.refid
+    }
+
     this.cartService.sendCartNotification(payload).subscribe((res: any) => {
-      if (res?.errorCode == 0) {
-        this.form.reset()
-      } else {
-        this.ToastrService.error('Coupon unavailable. Kindly add coupon')
+      if (res?.erroCode == 0) {
+
       }
     })
   }
