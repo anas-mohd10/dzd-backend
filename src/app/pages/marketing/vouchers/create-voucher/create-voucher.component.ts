1e49818a6ccb0139895462d1fd49763600c26fe3
Dheemanth Shenoy <dheemanth@webcastle.in>
2023-12-12 18:37:23 +0530

    update on 11/12

--- src/app/pages/marketing/vouchers/create-voucher/create-voucher.component.ts
+++ src/app/pages/marketing/vouchers/create-voucher/create-voucher.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { appRoutes } from 'src/app/config/routes';
+import { AppSettingsService } from 'src/app/includes/services/app.settings.service';
+import { CustomersService } from 'src/app/includes/services/customers.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-create-voucher',
@@ -6,10 +11,73 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-voucher.component.scss']
 })
 export class CreateVoucherComponent implements OnInit {
+  appRoute = appRoutes
+  form: FormGroup
+  customers: Array<any> = []
+  keyword: string;
+  optedCustomer: any
+  isToggle: boolean = false
+  settings: any
+  base: string = environment.base
+  isSubmitted: boolean = false
 
-  constructor() { }
+  constructor(
+    private CustomersService: CustomersService,
+    private ChangeDetectorRef: ChangeDetectorRef,
+    private AppSettingsService: AppSettingsService
+  ) { }
+
+  get formControls() {
+    return this.form.controls
+  }
 
   ngOnInit(): void {
+    this.getCustomers()
+
+    this.form = new FormGroup({
+      amount: new FormControl('', [Validators.required, Validators.pattern('^(0|[1-9]*)$')]),
+      name: new FormControl('', [Validators.required]),
+      email: new FormControl('', [Validators.required, Validators.email]),
+      countryCode: new FormControl('', [Validators.required, Validators.pattern('^(0|[1-9]*)$')]),
+      mobile: new FormControl('', [Validators.required, Validators.pattern('^(0|[1-9]*)$')]),
+      message: new FormControl('')
+    })
+
+    this.AppSettingsService.getGeneralSettingsbyId('1').subscribe((res: any) => {
+      if (res?.errorCode == 0) {
+        this.settings = res?.result
+        this.ChangeDetectorRef.markForCheck()
+      }
+    })
+  }
+
+  toggleDropdown() {
+    this.isToggle = !this.isToggle
+  }
+
+  getCustomers() {
+    if (this.keyword) {
+      this.CustomersService.searchCustomers({ page: 1, limit: 50, keyword: this.keyword }).subscribe({
+        next: (res: any) => {
+          if (res?.errorCode == 0) {
+            this.customers = res?.result?.data
+            this.ChangeDetectorRef.markForCheck()
+          } else {
+
+          }
+        }, error: (err: any) => {
+
+        }
+      })
+    } else {
+      this.customers = []
+    }
+  }
+
+  optCustomer(customer: any) {
+    this.customers = []
+    this.keyword = ''
+    this.optedCustomer = customer
   }
 
 }
--- src/app/pages/marketing/vouchers/vouchers-routing.module.ts
+++ src/app/pages/marketing/vouchers/vouchers-routing.module.ts
@@ -1,11 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { VouchersListComponent } from '../../sales/vouchers/vouchers-list/vouchers-list.component';
 import { CreateVoucherComponent } from './create-voucher/create-voucher.component';
 import { UpdateVoucherComponent } from './update-voucher/update-voucher.component';
+import { VoucherListingComponent } from './voucher-listing/voucher-listing.component';
 
 const routes: Routes = [
-  { path: '', component: VouchersListComponent },
+  { path: '', component: VoucherListingComponent },
   { path: 'create', component: CreateVoucherComponent },
   { path: 'update/:voucher', component: UpdateVoucherComponent }
 ];
