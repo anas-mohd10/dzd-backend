3a230862a8b4a3917aeda8bffff012a1f520d7c9
ankithaldar-plotx <103417385+ankithaldar-plotx@users.noreply.github.com>
2024-06-25 01:40:35 +0530

    dockerize with ci

---
+++ Dockerfile
@@ -0,0 +1,32 @@
+# Stage 1: Build the Angular application
+FROM node:18 AS build
+
+# Set the working directory
+WORKDIR /app
+
+# Copy package.json and package-lock.json
+COPY package*.json ./
+
+# Install dependencies
+RUN npm install --force
+
+# Copy the rest of the application source code
+COPY . .
+
+# Build the Angular application
+RUN npm run build --prod
+
+# Stage 2: Serve the application with Nginx
+FROM nginx:alpine
+
+# Copy the built application from the previous stage
+COPY --from=build /app/dist/commerce-castle /usr/share/nginx/html
+
+# # Copy the custom Nginx configuration file
+# COPY nginx.conf /etc/nginx/nginx.conf
+
+# Expose port 80
+EXPOSE 80
+
+# Start Nginx
+CMD ["nginx", "-g", "daemon off;"]
---
+++ sh.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Variables
+AWS_ACCOUNT_ID="211125565601"
+AWS_REGION="ap-south-1"
+ECR_REPO="admin"
+IMAGE_TAG="v0.0.3"  # e.g., v1.0.0
+
+# Login to AWS ECR
+aws ecr get-login-password --region ${AWS_REGION} | docker login --username AWS --password-stdin ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
+
+# Build the Docker image
+docker build -t ${ECR_REPO}:${IMAGE_TAG} .
+
+# Tag the Docker image for ECR
+docker tag ${ECR_REPO}:${IMAGE_TAG} ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO}:${IMAGE_TAG}
+
+# Push the Docker image to ECR
+docker push ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com/${ECR_REPO}:${IMAGE_TAG}
+
+# Logout from ECR
+docker logout ${AWS_ACCOUNT_ID}.dkr.ecr.${AWS_REGION}.amazonaws.com
