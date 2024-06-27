6d916c242bc75d9dd48fc386874e82275c1e1c82
Dheemanth Shenoy <dheemanth@webcastle.in>
2024-06-27 09:44:33 +0530

    latest changes in Menu Items

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -9,12 +9,12 @@ export const environment = {
   // base: 'https://ccapi.previewbay.com',
 
   //Local
-  // apiUrl: 'http://localhost:3000/api/v1/w/admin/auth',
-  // baseUrl: 'http://localhost:3000/api/v1/w/',
-  // appBaserl: 'http://localhost:3000/api/v1/a/',
+  apiUrl: 'http://localhost:3000/api/v1/w/admin/auth',
+  baseUrl: 'http://localhost:3000/api/v1/w/',
+  appBaserl: 'http://localhost:3000/api/v1/a/',
   // base: 'http://localhost:3000',
   base: 'https://storedadabucket.s3.ap-south-1.amazonaws.com',
-  //Local
+  // Local
 
   //Fresh fruit
   // apiUrl: 'https://freshfruitmart-api.storedada.net/api/v1/w/admin/auth',
@@ -31,9 +31,9 @@ export const environment = {
   //Vperfumes
 
   //Mattressland
-  apiUrl: 'https://mattressland-api.storedada.net/api/v1/w/admin/auth',
-  baseUrl: 'https://mattressland-api.storedada.net/api/v1/w/',
-  appBaserl: 'https://mattressland-api.storedada.net/api/v1/a/',
+  // apiUrl: 'https://mattressland-api.storedada.net/api/v1/w/admin/auth',
+  // baseUrl: 'https://mattressland-api.storedada.net/api/v1/w/',
+  // appBaserl: 'https://mattressland-api.storedada.net/api/v1/a/',
   // base: 'https://mattressland-api.storedada.net',
   //Mattressland
 
