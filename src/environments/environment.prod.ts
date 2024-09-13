6440bf47d63734bcb91cb735928392045ba16725
Dheemanth Shenoy <dshenoyd@gmail.com>
2024-09-14 00:15:45 +0530

    Invoice details added, coordinates in order details

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,9 +5,9 @@ export const environment = {
   isMockEnabled: true,
 
   // Local
-  apiUrl: 'http://localhost:3000/api/v1/w/admin/auth',
-  baseUrl: 'http://localhost:3000/api/v1/w/',
-  appBaserl: 'http://localhost:3000/api/v1/a/',
+  // apiUrl: 'http://localhost:3000/api/v1/w/admin/auth',
+  // baseUrl: 'http://localhost:3000/api/v1/w/',
+  // appBaserl: 'http://localhost:3000/api/v1/a/',
   base: '',
   // Local
 
@@ -34,9 +34,9 @@ export const environment = {
   // Sidco Foods
 
   // Fresh Market
-  // apiUrl: 'https://thefreshmarket-api.storedada.net/api/v1/w/admin/auth',
-  // baseUrl: 'https://thefreshmarket-api.storedada.net/api/v1/w/',
-  // appBaserl: 'https://thefreshmarket-api.storedada.net/api/v1/a/',
+  apiUrl: 'https://thefreshmarket-api.storedada.net/api/v1/w/admin/auth',
+  baseUrl: 'https://thefreshmarket-api.storedada.net/api/v1/w/',
+  appBaserl: 'https://thefreshmarket-api.storedada.net/api/v1/a/',
   // Fresh Market
   
     //Mamame
