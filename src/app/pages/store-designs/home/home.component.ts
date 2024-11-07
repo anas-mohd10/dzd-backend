51260087a96b1ab75c5e406c0c58005f10b80d55
karan <karan@webcastle.in>
2024-11-07 10:01:42 +0530

    new widgets like full container banner , mansory grid banner primary triple banner completed

--- src/app/pages/store-designs/home/home.component.ts
+++ src/app/pages/store-designs/home/home.component.ts
@@ -58,6 +58,11 @@ export class HomeComponent implements OnInit {
     { title: 'Aurora Grid', type: 'aurora-grid', icon: 'assets/widgets/aurora-grid.png', description: 'The following widget can be used to run a delivery timer with custom designs' },
     { title: 'Aurora Slider', type: 'aurora-slider', icon: 'assets/widgets/aurora-slider.png', description: 'The following widget can be used to show images within a particular category. The widget contains images.' },
     { title: 'Text Twirl', type: 'text-twirl', icon: 'assets/widgets/text-twirl.png', description: 'The following widget can be used to show limited set of medias with title and description. The widget contains images.' },
+    { title: 'Full Banner', type: 'full-banner', icon: 'assets/widgets/text-twirl.png', description: 'The following widget can be used to show limited set of medias with title and description. The widget contains images.' },
+    { title: 'Animation Banner', type: 'animation-banner', icon: 'assets/widgets/text-twirl.png', description: 'The following widget can be used to show limited set of medias with title and description. The widget contains images.' },
+    { title: 'Bricks Mansory Grid', type: 'brick-mansory-grid', icon: 'assets/widgets/text-twirl.png', description: 'The following widget can be used to show limited set of medias with title and description. The widget contains images.' },
+    { title: 'Primary Triple Grid', type: 'primary-triple-grid', icon: 'assets/widgets/text-twirl.png', description: 'The following widget can be used to show limited set of medias with title and description. The widget contains images.' },
+    
   ];
   homeWidgets: Array<any> = []
   homeWidgetKeyword: FormControl = new FormControl("", Validators.required)
@@ -92,7 +97,8 @@ export class HomeComponent implements OnInit {
     "grandeur-gallery", "celestial-canvas",
     "twin-towers", "stellar-selections",
     "slider-spotlight", "trending-teasers",
-    "text-twirl"
+    "text-twirl", "full-banner","animation-banner",
+    "brick-mansory-grid","primary-triple-grid"
   ]
   isWidgetLoaded: boolean = false
   redirectionItems: Array<any> = [
