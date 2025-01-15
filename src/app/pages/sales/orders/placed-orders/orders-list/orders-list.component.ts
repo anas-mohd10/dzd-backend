4fa3d508773721176865d44a2e7b1ff3ada2c160
Shyam <shyam@webcastle.in>
2025-01-15 12:17:12 +0530

    date format old code reverted

--- src/app/pages/sales/orders/placed-orders/orders-list/orders-list.component.ts
+++ src/app/pages/sales/orders/placed-orders/orders-list/orders-list.component.ts
@@ -411,23 +411,31 @@ export class OrdersListComponent implements OnInit {
     window.open(`${this.packingSlipUrl}${queryString}`, '_blank')
   }
 
-  formatDate(date: string) {
-  return new Intl.DateTimeFormat('en-US', {
-    year: 'numeric',
-    month: 'short',
-    day: 'numeric',
-    timeZone: 'UTC', // Force UTC timezone
-  }).format(new Date(date));
-}
+  formatTime(time: string) {
+    return new Date(time).toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', hour12: true })
+  }
 
-formatTime(time: string) {
-  return new Intl.DateTimeFormat('en-US', {
-    hour: 'numeric',
-    minute: 'numeric',
-    hour12: true,
-    timeZone: 'UTC', // Force UTC timezone
-  }).format(new Date(time));
-}
+  formatDate(date: string) {
+    return new Date(date).toLocaleDateString('en-US', { year: 'numeric', month: 'short', day: 'numeric' })
+  }
+
+//   formatDate(date: string) {
+//   return new Intl.DateTimeFormat('en-US', {
+//     year: 'numeric',
+//     month: 'short',
+//     day: 'numeric',
+//     timeZone: 'UTC', // Force UTC timezone
+//   }).format(new Date(date));
+// }
+
+// formatTime(time: string) {
+//   return new Intl.DateTimeFormat('en-US', {
+//     hour: 'numeric',
+//     minute: 'numeric',
+//     hour12: true,
+//     timeZone: 'UTC', // Force UTC timezone
+//   }).format(new Date(time));
+// }
 
   exportOrders() {
     this.OrdersService.exportOrderTabs({
