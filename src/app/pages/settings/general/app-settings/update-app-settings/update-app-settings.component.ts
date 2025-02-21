420ba4b0e2fdd768534a17e51d5dc596341980e5
Shyam <shyam@webcastle.in>
2025-02-21 14:44:38 +0530

    delivery slots buffer time

--- src/app/pages/settings/general/app-settings/update-app-settings/update-app-settings.component.ts
+++ src/app/pages/settings/general/app-settings/update-app-settings/update-app-settings.component.ts
@@ -291,6 +291,8 @@ export class UpdateAppSettingsComponent implements OnInit {
         this.form.get('defaultBanner')?.setValue(res?.result?.defaultBanner)
         this.form.get('defaultMobileBanner')?.setValue(res?.result?.defaultMobileBanner)
 
+        this.form.get('deliverSlotBufferTime')?.setValue(res?.result?.deliverSlotBufferTime || 60);
+
         this.form.get('commaSeparation')?.setValue(
           res?.result?.commaSeparation ?? true  // Use nullish coalescing for default
         );
@@ -384,6 +386,7 @@ export class UpdateAppSettingsComponent implements OnInit {
       defaultMobileBanner: [''],
       defaultShippingCharge: ['0'],
       defaultMinimumCartAmount: ['0'],
+      deliverSlotBufferTime: [60],
     })
   }
 
@@ -536,6 +539,7 @@ export class UpdateAppSettingsComponent implements OnInit {
       offerCriteria: this.form.get('offerCriteria')?.value,
       isStoreLive: this.form.get('isStoreLive')?.value,
       notes: { packingSlip: this.form.get('packingSlip')?.value },
+      deliverSlotBufferTime: this.form.get('deliverSlotBufferTime')?.value || 60,
       commaSeparation: Boolean(this.form.get('commaSeparation')?.value),
       decimalValues: Number(this.form.get('decimalValues')?.value),
       buttons: {
