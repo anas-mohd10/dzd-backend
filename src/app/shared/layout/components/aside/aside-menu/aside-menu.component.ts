59a4e8925e0ab6cd2ff8a0d72f8271a9d544f07e
Ramesh P R <ramesh@webcastle.in>
2025-03-03 17:06:26 +0530

    profile click issue ressolved

--- src/app/shared/layout/components/aside/aside-menu/aside-menu.component.ts
+++ src/app/shared/layout/components/aside/aside-menu/aside-menu.component.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, TemplateRef, ViewChild } from '@angular/core';
 import { environment } from '../../../../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { appRoutes } from 'src/app/config/routes';
+import { Router } from '@angular/router';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
 
 @Component({
   selector: 'app-aside-menu',
@@ -11,7 +13,14 @@ import { appRoutes } from 'src/app/config/routes';
 
 export class AsideMenuComponent implements OnInit {
   appRoute = appRoutes;
-  constructor(private http: HttpClient) { }
+  modalRef?: BsModalRef;
+  @ViewChild('logoutModal') logoutModal!: TemplateRef<any>;
+
+  constructor(
+    private http: HttpClient, 
+    private router: Router,
+    private modalService: BsModalService
+  ) { }
 
   rootUrl = "https://localhost:3000/api/v1/w/admin/auth";
 
@@ -21,6 +30,27 @@ export class AsideMenuComponent implements OnInit {
     this.http.get(this.rootUrl + "/roles").subscribe((_res) => {
       return _res
     })
+  }
+
+  // Update the logout click handler to show modal
+  logout() {
+    this.modalRef = this.modalService.show(this.logoutModal, {
+      class: 'modal-dialog-centered'
+    });
+  }
+
+  // Confirm logout
+  confirmLogout() {
+    localStorage.removeItem('access-token');
+    localStorage.removeItem('UserData');
+    localStorage.removeItem('is_logged_in');
+    this.modalRef?.hide();
+    this.router.navigate(['/auth/login']);
+  }
+
+  // Decline logout
+  declineLogout() {
+    this.modalRef?.hide();
   }
 
   getPermissions() {
