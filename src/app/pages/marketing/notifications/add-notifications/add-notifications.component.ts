5724bd8fd0fd770216c1c2270fc6f7812cdc9398
Dheemanth Shenoy <dshenoyd@gmail.com>
2025-06-26 10:06:45 +0530

    feat(add-notifications): enhance notification creation form with customer selection and pagination
    Updated the add notifications component to include a modal for selecting customers, improved form validation, and added pagination for customer retrieval. Enhanced the UI for better user experience, including new styles for tables and form elements. Removed unnecessary fields from the notifications list filter.

--- src/app/pages/marketing/notifications/add-notifications/add-notifications.component.ts
+++ src/app/pages/marketing/notifications/add-notifications/add-notifications.component.ts
@@ -1,10 +1,20 @@
-import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit, TemplateRef } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { HotToastService } from '@ngneat/hot-toast';
+import { BsModalRef, BsModalService } from 'ngx-bootstrap/modal';
+import { debounceTime } from 'rxjs/operators';
 import { appRoutes } from 'src/app/config/routes';
 import { CustomersService } from 'src/app/includes/services/customers.service';
 import { NotificationsService } from 'src/app/includes/services/notifications.service';
+
+interface CustomerDoc {
+  name: string;
+  email: string;
+  countryCode: string;
+  _id: string;
+  mobile: string;
+}
 @Component({
   selector: 'app-add-notifications',
   templateUrl: './add-notifications.component.html',
@@ -14,63 +24,90 @@ export class AddNotificationsComponent implements OnInit {
   form: FormGroup;
   isSubmitted: boolean;
   isLoading: boolean = false;
-  appRoute = appRoutes;
-  customersData: Array<any> = [];
-  customers: any;
-  startDate: string = new Date().toISOString().split('T')[0];
-  date = new Date();
-  formattedDate: number = this.date.setDate(this.date.getDate() + 2);
-  scheduleDate: string = this.date.toISOString().split('T')[0];
-  thumbnail: string = '';
+  appRoute = appRoutes;  
+  modalRef: BsModalRef;
+  page: number = 1;
+  limit: number = 10;
+  totalResults: number = 0;
+  totalPages: number = 1;
+  customerDocs: Array<CustomerDoc> = [];
+  customers: Array<CustomerDoc> = [];
+  searchKeyword: FormControl = new FormControl('');
 
   constructor(
     private NotificationsService: NotificationsService,
     private CustomersService: CustomersService,
     private Router: Router,
     private HotToastService: HotToastService,
-    private ChangeDetectorRef: ChangeDetectorRef
-  ) { }
+    private ChangeDetectorRef: ChangeDetectorRef,
+    private BsModalService: BsModalService
+  ) {
+    this.searchKeyword.valueChanges.pipe(debounceTime(500)).subscribe((value) => {
+      this.getCustomers()
+    })
+  }
 
-  ngOnInit(): void {
-    this.form = new FormGroup({
-      title: new FormControl('', Validators.required),
-      channel: new FormControl('', Validators.required),
-      type: new FormControl('instant'),
-      content: new FormControl('', Validators.required),
-      scheduledAt: new FormControl(''), // Combined date and time
-      redirection: new FormControl(''),
-      thumbnail: new FormControl(null),
-      isStoreLevel: new FormControl('true', Validators.required),
-      isActive: new FormControl('true', Validators.required),
-    });
+  //Open modal
+  open(template: TemplateRef<any>) {
+    this.modalRef = this.BsModalService.show(template, { class: 'modal-lg', ignoreBackdropClick: true });
+  }
 
-    this.form.get('scheduledDate')?.setValue(this.scheduleDate); // set default schedule date
+  //Pagination
+  onPageTriggered(event: { pageIndex: number, pageSize: number }) {
+    this.page = event.pageIndex
+    this.limit = event.pageSize
+    this.getCustomers()
+  }
 
-    this.CustomersService.getActiveCustomers().subscribe({
+  //Get customers
+  getCustomers() {
+    this.CustomersService.searchCustomers({
+      keyword: this.searchKeyword.value,
+      page: this.page,
+      isActive: true,
+      limit: this.limit
+    }).subscribe({
       next: (res: any) => {
         if (res?.errorCode == 0) {
-          this.customersData = res?.result;
-          for (let customer of this.customersData)
-            customer.title =
-              (customer?.name ? customer?.name : '-- Incomplete Profile --') +
-              ' ( ' +
-              customer?.mobile +
-              ' )';
-          this.ChangeDetectorRef.markForCheck();
-        }
+          this.customerDocs = res?.result?.data
+          this.totalResults = res?.result?.totalResults
+          this.totalPages = res?.result?.totalPages
+          this.ChangeDetectorRef.markForCheck()
+        } else { }
       },
       error: (err: any) => { },
     });
   }
 
+  onSelect(customer: CustomerDoc) {
+    if (!this.isCustomerSelected(customer)) {
+      this.customers.push(customer);
+    } else {
+      this.customers = this.customers.filter(c => c._id != customer._id);
+    }
+  }
+
+  isCustomerSelected(customer: CustomerDoc) {
+    return this.customers.find(c => c._id == customer._id);
+  }
+
+  ngOnInit(): void {
+    this.form = new FormGroup({
+      title: new FormControl('', Validators.required),
+      content: new FormControl('', Validators.required),
+      scheduledAt: new FormControl('', Validators.required),
+      isStoreLevel: new FormControl('true'),
+      redirection: new FormControl(''),
+      thumbnail: new FormControl(''),
+    });
+  }
+
   handleThumbnail(event: any) {
-    this.form.get('thumbnail')?.setValue(event?._id);
-    this.thumbnail = event?.path;
+    this.form.get('thumbnail')?.setValue(event?.path);
   }
 
   removeThumbnail() {
-    this.form.get('thumbnail')?.setValue(null);
-    this.thumbnail = '';
+    this.form.get('thumbnail')?.setValue('');
   }
 
   get formControls() {
@@ -88,6 +125,12 @@ export class AddNotificationsComponent implements OnInit {
     }
 
     this.isLoading = true;
+
+    if(this.form.get('isStoreLevel')?.value == 'false' && this.customers.length == 0) {
+      this.HotToastService.error('Please select at least one customer');
+      this.isLoading = false;
+      return;
+    }
 
     this.NotificationsService.addNotification({
       ...this.form.value,
